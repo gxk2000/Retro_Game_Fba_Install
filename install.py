e2c0abe9777936645a06007febea10babc6a00b3
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-08-30 20:31:46 +0800

    Add files via upload

--- install.py
+++ install.py
@@ -2,8 +2,23 @@
 import os
 import shutil
 
+# 清除上次安装
+if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png"):
+    os.system("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
+if os.path.exists("/home/cpi/apps/emulators/fbalpha2012_libretro.so"):
+    os.system("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+if os.path.exists("/home/cpi/apps/Menu/20_Retro Games/11_FBA"):
+    os.system("/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+
+# 安装
 shutil.move("/home/cpi/Retro_Game_Fba_Install/FBA.png", "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
 shutil.move("/home/cpi/Retro_Game_Fba_Install/fbalpha2012_libretro.so", "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
 shutil.move("/home/cpi/Retro_Game_Fba_Install/11_FBA", "/home/cpi/apps/Menu/20_Retro Games/11_FBA")
-os.system("rm -rf /home/cpi/Retro_Game_Fba_Install")
-print("----- FBA模拟器安装完成！ -----")
+
+# 提示
+if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png"):
+    if os.path.exists("/home/cpi/apps/emulators/fbalpha2012_libretro.so"):
+        if os.path.exists("/home/cpi/apps/Menu/20_Retro Games/11_FBA"):
+            print("----- FBA模拟器安装完成！ -----")
+else:
+    print("----- FBA模拟器安装失败！ -----")
---
+++ uninstall.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+import os
+
+if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png"):
+    os.system("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
+
+if os.path.exists("/home/cpi/apps/emulators/fbalpha2012_libretro.so"):
+    os.system("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+
+if os.path.exists("/home/cpi/apps/Menu/20_Retro Games/11_FBA"):
+    os.system("/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+
+print("----- fba模拟器已删除 -----")
