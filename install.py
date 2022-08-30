937152bb9deaa4e5e6bc3f370a6176a763c9e759
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-08-30 21:05:43 +0800

    Add files via upload

--- install.py
+++ install.py
@@ -2,16 +2,15 @@
 import os
 import shutil
 
-# 获取root
-os.system("su")
-
 # 清除上次安装
 if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png"):
-    os.system("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
+    os.remove("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
 if os.path.exists("/home/cpi/apps/emulators/fbalpha2012_libretro.so"):
-    os.system("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+    os.remove("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
 if os.path.exists("/home/cpi/apps/Menu/20_Retro Games/11_FBA"):
-    os.system("/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+    os.removedirs("/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+if os.path.exists("/home/cpi/games/FBA"):
+    os.removedirs("/home/cpi/games/FBA")
 
 # 安装
 shutil.move("/home/cpi/Retro_Game_Fba_Install/FBA.png", "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
--- uninstall.py
+++ uninstall.py
@@ -1,16 +1,16 @@
 # -*- encoding: utf-8 -*-
 import os
 
-os.system("su")
-
-
-if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/'20_Retro Games'/FBA.png"):
-    os.system("/home/cpi/launcher/skin/default/Menu/GameShell/'20_Retro Games'/FBA.png")
+if os.path.exists("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png"):
+    os.remove("/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
 
 if os.path.exists("/home/cpi/apps/emulators/fbalpha2012_libretro.so"):
-    os.system("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+    os.remove("/home/cpi/apps/emulators/fbalpha2012_libretro.so")
 
-if os.path.exists("/home/cpi/apps/Menu/'20_Retro Games'/11_FBA"):
-    os.system("/home/cpi/apps/Menu/'20_Retro Games'/11_FBA")
+if os.path.exists("/home/cpi/apps/Menu/20_Retro Games/11_FBA"):
+    os.removedirs("/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+
+if os.path.exists("/home/cpi/games/FBA"):
+    os.removedirs("/home/cpi/games/FBA")
 
 print("----- fba模拟器已删除 -----")
