b7d24b67253e52e73b4b62ed4c9085609e66c56e
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-08-30 18:52:55 +0800

    Delete Retro_Game_Fba_Install directory

--- Retro_Game_Fba_Install/install.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-import shutil
-
-shutil.move('FBA.png', "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
-shutil.move('fbalpha2012_libretro.so', "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
-shutil.move('11_FBA', "/home/cpi/apps/Menu/20_Retro Games/11_FBA")
-
-print("----- FBA安装完成！ -----")
