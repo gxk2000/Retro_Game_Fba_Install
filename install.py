d6cb8ab332dae38a46608d8993c4129675eeeaa1
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-08-30 18:59:28 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,11 @@
 # -*- encoding: utf-8 -*-
+import os
 import shutil
 
+print("----- FBA模拟器开始安装！ -----")
+os.system('cd /home/cpi/Retro_Game_Fba_Install')
 shutil.move('FBA.png', "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
-shutil.move('fbalpha2012_libretro.so', "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+shutil.move("fbalpha2012_libretro.so", "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
 shutil.move('11_FBA', "/home/cpi/apps/Menu/20_Retro Games/11_FBA")
-
-print("----- FBA安装完成！ -----")
+os.system('rm - rf /home/cpi/Retro_Game_Fba_Install')
+print("----- FBA模拟器安装完成！ -----")
