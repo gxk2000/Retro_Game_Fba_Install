a09aff0b468ab9f5bd6878f95fbf824652c8cd2d
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-08-30 19:04:17 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,13 +1,10 @@
 # -*- encoding: utf-8 -*-
-#pay attention this project was base in python2
-
 import os
 import shutil
 
 print("----- FBA模拟器开始安装！ -----")
-os.system('cd /home/cpi/Retro_Game_Fba_Install')
-shutil.move('FBA.png', "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
-shutil.move("fbalpha2012_libretro.so", "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
-shutil.move('11_FBA', "/home/cpi/apps/Menu/20_Retro Games/11_FBA")
-os.system('rm - rf /home/cpi/Retro_Game_Fba_Install')
+shutil.move("/home/cpi/Retro_Game_Fba_Install/FBA.png", "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/FBA.png")
+shutil.move("/home/cpi/Retro_Game_Fba_Install/fbalpha2012_libretro.so", "/home/cpi/apps/emulators/fbalpha2012_libretro.so")
+shutil.move("/home/cpi/Retro_Game_Fba_Install/11_FBA", "/home/cpi/apps/Menu/20_Retro Games/11_FBA")
+os.system("rm - rf /home/cpi/Retro_Game_Fba_Install")
 print("----- FBA模拟器安装完成！ -----")
